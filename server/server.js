b56e11c8a62982a808ada2de11b38162c423155e
junqing258@gmail.com <junqing258@gmail.com>
2020-03-17 09:59:45 +0800

    homogeny

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 // https://blog.csdn.net/DeepLies/article/details/78007731
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const { render } = require('./render');
 // const { render } = require('../../dist/ssr');
+
+let assetManifest;
+
+const getAsset = (name) => {
+  const assetsFile = path.resolve(__dirname, '../dist/asset-manifest.json');
+  if (fs.existsSync(assetsFile)) {
+    assetManifest = JSON.parse(fs.readFileSync(assetsFile));
+  }
+  if (assetManifest) name = assetManifest[name];
+  return name;
+}
 
 const server = express();
 server.use(cookieParser());
@@ -27,11 +39,11 @@ server.use((req, res, next) => {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
       <title>平安好医生</title>
-      <link rel="stylesheet" type="text/css" href="dist/app.css">
+      <link rel="stylesheet" type="text/css" href="dist/${getAsset('app.css')}">
     </head>
     <body>
       <div id="root">${render(req, res)}</div>
-      <script type="text/javascript" src="dist/app.js"></script>
+      <script type="text/javascript" src="dist/${getAsset('app.js')}"></script>
     </body>
   </html>`;
   res.send(pageHtml);
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.hydration(<App />, document.getElementById('root'));
 
 // import routes from './routes';
 // const App = () => <BrowserRouter>{routes}</BrowserRouter>;
